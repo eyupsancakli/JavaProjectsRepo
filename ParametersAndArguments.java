d5b6aea0143778da6c3e7f2404fcfb82e0383210
Eyup Sancakli <82240491+eyupsancakli@users.noreply.github.com>
2026-02-13 10:49:42 +0000

    "ParametersAndArgumentsv1.0"

---
+++ ParametersAndArguments.java
@@ -0,0 +1,22 @@
+///Parameters are the veriables in a method. 
+/// 
+/// 
+public static int add (int a, int b){
+     return (a + b);
+/// integer a and b are paramenters. Basically there are empty boxes waiting for value when method called. 
+/// Lets create Argument for related parameters.
+/// 
+}
+
+///Arguments (The Actual Value)
+/// 
+ int result add(3,5);
+
+ /// here 3 and 5 are arguments.
+ /// Quick memory trick
+ /// Parameters = method definition
+ /// Arguments = method call
+
+
+
+
